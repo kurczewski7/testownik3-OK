96323ee2ae00a385fa000a09e70a7899f6619b2d
Slawomir Kurczewski <kurczewski7@gmail.com>
2022-01-11 23:30:16 +0100

    Next

---
+++ RunMeBefor.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#This is sample program 
+var=$(date +"%Y-%m-%dT%H:%M:%S%z")
+echo $var # Prints an ISO-8601 compliant date with time-zone
+cho After download testownik and execute command:
+echo sudo gem install cocoa pod
+
+Echo Install the pod: pod install
+pod install
